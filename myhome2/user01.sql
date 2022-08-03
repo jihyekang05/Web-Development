d45c30c6a5dcd56baf6fc86faf38a7631f1e014a
JIHYE KANG <62206966+jihyekang05@users.noreply.github.com>
2022-08-03 17:09:48 +0900

    Update user01.sql

--- myhome2/user01.sql
+++ myhome2/user01.sql
@@ -12,3 +12,22 @@ insert into board values('2','제목2','내용2','작성자3',sysdate);
 insert into board values('3','제목3','내용2','작성자3',sysdate);
 
 commit;
+
+create table grade(
+id number(10) primary key,
+name varchar2(10),
+kor number(10),
+math number(10),
+eng number(10),
+grade_sum number(30),
+grade_avg number(30));
+
+insert into grade values(1,'민수',50,50,50,150,50);
+
+insert into grade values(2,'지혜',60,90,60,210,70);
+
+commit;
+
+
+create sequence boardSeq;
+select boardSeq.nextVal from dual;
